c1d510db94486417d2dfa1b15dd26412462875c5
YoungTr <tangyan1004@gmail.com>
2021-01-22 20:23:19 +0800

    sort temple

--- src/com/algs4/sort/Example.java
+++ src/com/algs4/sort/Example.java
@@ -6,7 +6,41 @@
 
 package com.algs4.sort;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Example {
+
+    public static void sort(Comparable[] a) {
+
+    }
+
+    public static boolean less(Comparable v, Comparable m) {
+        return v.compareTo(m) < 0;
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+
     public static void main(String[] args) {
 
     }
